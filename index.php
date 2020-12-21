e7fb86866718fb3a93be0308f3c5165d1f657483
unknown <moataz180325@fci.bu.edu.eg>
2020-12-21 22:59:35 +0200

    Add Few of Work

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Double-M Portfolio Maker</title>
+        <title>Double M - Portfolio Maker</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">        
+        <link rel="stylesheet" href="css/all.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css">
@@ -45,7 +46,7 @@
                     <div class="col-6 pt-5">
                         <h1 class = "display-4 white"><span class = "custom">Creative </span>Agency</h1>
                         <p class = "lead text-light">Create Your own Potfolio With us.</p>
-                        <p class = "custom-p">This For Youth Web Developers To make Own Portfolio Free with Full Customized Components.</p>
+                        <p class = "custom-p">Double-M Software For Youth Web Developers To make Own Portfolio, Free with Full Customized Components.</p>
                         <button class = "btn">Learn More</button>
                     </div>
                     <div class="col-6">
@@ -53,10 +54,51 @@
                     </div>
                 </div>
             </div>
+            <!-- More Info Section -->
+            <div class="info">
+                <div class="row pt-5">
+                    <div class="col-8">
+                        <h2 class = "display-5 text-light">About <span class = "custom">Double-M</span> Company..?</h2>
+                        <p class="lead text-light pt-3">
+                            Egyptian Software Development Company Owned By Mr. Moataz Ahmed is a Full Stack Web Developer Graduated From Faculty Of Computer Science Benha University have 2 Years Experience In Web Development.
+                        </p>
+                        <a href="https://www.linkedin.com/in/moataz2707/" target = "_blank" class = "text-muted"><i class="fas fa-external-link-alt mr-2"></i>Learm more about Moataz</a>
+                    </div>
+                    <div class="col-4">
+                        <img class = "w-75 img-fluid rounded-circle" src="img/moataz.jpg" alt="Moataz">
+                    </div>
+                </div>
+            </div>
+            <!-- Testmonials Section -->
+            <div class="testmonials">
+                <h2 class = " text-light mb-5">What Customers Say About US</h2>
+                <div id="demo" class="carousel slide" data-ride="carousel">
+                    <ul class="carousel-indicators">
+                        <li data-target="#demo" data-slide-to="0" class="active"></li>
+                        <li data-target="#demo" data-slide-to="1"></li>
+                        <li data-target="#demo" data-slide-to="2"></li>
+                    </ul>
+                    <div class="carousel-inner text-center">
+                        <div class="carousel-item active mb-5">
+                            <img class = "img-fluid" src="img/boss.svg">
+                            <p class="lead text-light">This is Best Website</p>
+                        </div>
+                        <div class="carousel-item mb-5">
+                            <img class = "img-fluid" src="img/girl.svg">
+                            <p class="lead text-light">This is Best Website</p>
+                        </div>
+                        <div class="carousel-item mb-5">
+                            <img class = "img-fluid" src="img/boss.svg">
+                            <p class="lead text-light">This is Best Website</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="js/all.min.js"></script>
         <script src="js/main.js"></script>
     </body>
 </html>
