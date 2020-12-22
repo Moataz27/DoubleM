4ff02ec95e008658556ce721e16f315f2d7cb4bd
unknown <moataz180325@fci.bu.edu.eg>
2020-12-22 21:17:52 +0200

    Add New Features

--- js/main.js
+++ js/main.js
@@ -10,5 +10,23 @@ $(function(){
                 window.location.hash = hash;
             });
         }
+        if($(this).parent('.nav-item')){
+            $(this).parent().addClass('active').siblings().removeClass('active');
+        }
+        
+    });
+    
+    var scrollBtn = $('.scrollTop');
+    $(window).on("scroll", function(){
+        if(scrollY >= 300){
+            $(scrollBtn).fadeIn();
+        } else{
+            $(scrollBtn).fadeOut();
+        }
+    });
+    $(scrollBtn).on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        });
     });
 });
